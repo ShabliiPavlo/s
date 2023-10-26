8a4e13b7f20e75b4ff8e748f6b4f2ae261cfd183
ShabliiPavlo <shabliypavel@icloud.com>
2023-10-26 12:37:58 +0300

    Initial Commit

---
+++ s/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  s
+//
+//  Created by Pavel Shabliy on 26.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
